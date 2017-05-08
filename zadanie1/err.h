9a31eb8382c22963726ea0b67e94d75f34ff3208
Łukasz Raszkiewicz <lraszkiewicz@gmail.com>
2017-05-08 12:08:11 +0200

    Initial commit

---
+++ zadanie1/err.h
@@ -0,0 +1,35 @@
+#ifndef _ERR_
+#define _ERR_
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+/* Wypisuje informację o błędnym zakończeniu funkcji systemowej
+i kończy działanie programu. */
+void syserr(const char *fmt, ...) {
+  va_list fmt_args;
+
+  fprintf(stderr, "ERROR: ");
+  va_start(fmt_args, fmt);
+  vfprintf(stderr, fmt, fmt_args);
+  va_end(fmt_args);
+  fprintf(stderr, " (%d; %s)\n", errno, strerror(errno));
+  exit(EXIT_FAILURE);
+}
+
+/* Wypisuje informację o błędzie i kończy działanie programu. */
+void fatal(const char *fmt, ...) {
+  va_list fmt_args;
+
+  fprintf(stderr, "ERROR: ");
+  va_start(fmt_args, fmt);
+  vfprintf(stderr, fmt, fmt_args);
+  va_end(fmt_args);
+  fprintf(stderr, "\n");
+  exit(EXIT_FAILURE);
+}
+
+#endif
